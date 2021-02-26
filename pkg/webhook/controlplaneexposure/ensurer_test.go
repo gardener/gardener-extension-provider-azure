632069d17d54cd5b1689ce2bb28feccedc240e6f
Kristiyan Gostev <kristiyan.gostev@sap.com>
2021-02-26 15:26:14 +0200

    Vendor g/g@1.17.1 and terraformer config cleanup

--- pkg/webhook/controlplaneexposure/ensurer_test.go
+++ pkg/webhook/controlplaneexposure/ensurer_test.go
@@ -145,7 +145,7 @@ var _ = Describe("Ensurer", func() {
 			// Create mock client
 			c := mockclient.NewMockClient(ctrl)
 			c.EXPECT().Get(context.TODO(), client.ObjectKey{Name: namespace}, &extensionsv1alpha1.Cluster{}).DoAndReturn(clientGet(cluster))
-			c.EXPECT().Get(context.TODO(), svcKey, &corev1.Service{}).DoAndReturn(clientGet(svc))
+			c.EXPECT().Get(context.TODO(), svcKey, gomock.AssignableToTypeOf(&corev1.Service{})).DoAndReturn(clientGet(svc))
 
 			// Create ensurer
 			ensurer := NewEnsurer(etcdStorage, logger)
@@ -180,7 +180,7 @@ var _ = Describe("Ensurer", func() {
 			// Create mock client
 			c := mockclient.NewMockClient(ctrl)
 			c.EXPECT().Get(context.TODO(), client.ObjectKey{Name: namespace}, &extensionsv1alpha1.Cluster{}).DoAndReturn(clientGet(cluster))
-			c.EXPECT().Get(context.TODO(), svcKey, &corev1.Service{}).DoAndReturn(clientGet(svc))
+			c.EXPECT().Get(context.TODO(), svcKey, gomock.AssignableToTypeOf(&corev1.Service{})).DoAndReturn(clientGet(svc))
 
 			// Create ensurer
 			ensurer := NewEnsurer(etcdStorage, logger)
