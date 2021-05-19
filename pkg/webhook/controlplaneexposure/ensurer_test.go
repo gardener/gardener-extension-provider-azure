d19d4a750950ffdd3ee61e589a5622d2db283148
Kistner, Dominic <dominic.kistner@sap.com>
2021-05-19 13:41:24 +0200

    Do not ensure apiserver flag `external-hostname`

--- pkg/webhook/controlplaneexposure/ensurer_test.go
+++ pkg/webhook/controlplaneexposure/ensurer_test.go
@@ -311,7 +311,6 @@ func checkKubeAPIServerDeployment(dep *appsv1.Deployment) {
 	c := extensionswebhook.ContainerWithName(dep.Spec.Template.Spec.Containers, "kube-apiserver")
 	Expect(c).To(Not(BeNil()))
 	Expect(c.Command).To(ContainElement("--advertise-address=1.2.3.4"))
-	Expect(c.Command).To(ContainElement("--external-hostname=1.2.3.4"))
 }
 
 func checkETCDMain(etcd *druidv1alpha1.Etcd) {
