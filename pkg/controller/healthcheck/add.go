58130a9502139b2d6457eba4fbf145cb32c359c7
Shafeeque E S <shafeeque.e.s@sap.com>
2022-08-09 08:22:19 +0530

    Vendor `gardener@1.52.2` and Run `make generate`

--- pkg/controller/healthcheck/add.go
+++ pkg/controller/healthcheck/add.go
@@ -19,10 +19,10 @@ import (
 	"time"
 
 	"github.com/gardener/gardener-extension-provider-azure/pkg/azure"
+	healthcheckconfig "github.com/gardener/gardener/extensions/pkg/apis/config"
 	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
 	genericcontrolplaneactuator "github.com/gardener/gardener/extensions/pkg/controller/controlplane/genericactuator"
 	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck"
-	healthcheckconfig "github.com/gardener/gardener/extensions/pkg/controller/healthcheck/config"
 	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck/general"
 	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck/worker"
 	genericworkeractuator "github.com/gardener/gardener/extensions/pkg/controller/worker/genericactuator"
