5851cdc8ffe919464ecff403b26a31693dffbac1
Adrian Stobbe <adrian.stobbe@sap.com>
2022-11-02 11:23:44 +0100

    Revert "Revert "add resourcegroup client""
    This reverts commit 33ef53939cdb32e1aa983c15737c3cd8b8fe9435.

--- pkg/azure/client/types.go
+++ pkg/azure/client/types.go
@@ -19,6 +19,7 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v4"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2021-03-01/compute"
 	"github.com/Azure/azure-sdk-for-go/services/dns/mgmt/2018-05-01/dns"
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2020-05-01/network"
@@ -43,6 +44,12 @@ type Factory interface {
 	NetworkInterface(ctx context.Context, secretRef corev1.SecretReference) (NetworkInterface, error)
 	Disk(ctx context.Context, secretRef corev1.SecretReference) (Disk, error)
 	Subnet(ctx context.Context, secretRef corev1.SecretReference) (Subnet, error)
+	ResourceGroup(ctx context.Context, secretRef corev1.SecretReference) (ResourceGroup, error)
+}
+
+type ResourceGroup interface {
+	CreateOrUpdate(ctx context.Context, resourceGroupName, location string) error
+	Delete(ctx context.Context, resourceGroupName string) error
 }
 
 // Group represents an Azure group client.
@@ -150,6 +157,11 @@ type VmssClient struct {
 	client compute.VirtualMachineScaleSetsClient
 }
 
+type ResourceGroupClient struct {
+	client *armresources.ResourceGroupsClient
+}
+
+// VnetClient is an implmenetation of Vnet for a virtual network client.
 type VnetClient struct {
 	client *armnetwork.VirtualNetworksClient
 }
