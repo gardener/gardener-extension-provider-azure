9e010914235368e4b452bc2ac501c580d2add06a
Adrian Stobbe <adrian.stobbe@sap.com>
2022-11-02 11:23:44 +0100

    Revert "Revert "add resourcegroup client""
    This reverts commit 33ef53939cdb32e1aa983c15737c3cd8b8fe9435.

--- pkg/azure/client/resourcegroup.go
+++ pkg/azure/client/resourcegroup.go
@@ -3,45 +3,23 @@ package client
 import (
 	"context"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
-	"github.com/gardener/gardener-extension-provider-azure/pkg/internal"
 )
 
-// TODO move to group.go?
-
-func CreateOrUpdateResourceGroup(ctx context.Context, resourceGroupName, location string, clientAuth internal.ClientAuth) (*armresources.ResourceGroup, error) {
-	cred, err := clientAuth.GetAzClientCredentials()
-	if err != nil {
-		return nil, err
-	}
-	resourceGroupClient, err := armresources.NewResourceGroupsClient(clientAuth.SubscriptionID, cred, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resourceGroupResp, err := resourceGroupClient.CreateOrUpdate(
+func (c ResourceGroupClient) CreateOrUpdate(ctx context.Context, resourceGroupName, location string) error {
+	_, err := c.client.CreateOrUpdate(
 		ctx,
 		resourceGroupName,
 		armresources.ResourceGroup{
 			Location: to.Ptr(location),
 		},
 		nil)
-	if err != nil {
-		return nil, err
-	}
-	return &resourceGroupResp.ResourceGroup, nil
+	return err
 }
 
-// sequential delete
-func DeleteResourceGroup(ctx context.Context, resourceGroupName, location, subsriptionID string, cred azcore.TokenCredential) error {
-	resourceGroupClient, err := armresources.NewResourceGroupsClient(subsriptionID, cred, nil)
-	if err != nil {
-		return err
-	}
-
-	resourceGroupResp, err := resourceGroupClient.BeginDelete(
+func (c ResourceGroupClient) Delete(ctx context.Context, resourceGroupName string) error {
+	resourceGroupResp, err := c.client.BeginDelete(
 		ctx,
 		resourceGroupName,
 		nil)
