341173025dff706f92d352fb9782ba557255d520
Adrian Stobbe <adrian.stobbe@sap.com>
2022-11-03 16:02:20 +0100

    add shared infraflow + build graph

--- pkg/controller/infrastructure/infraflow/reconciler.go
+++ pkg/controller/infrastructure/infraflow/reconciler.go
@@ -2,13 +2,16 @@ package infraflow
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2"
 	"github.com/gardener/gardener-extension-provider-azure/pkg/apis/azure"
 	"github.com/gardener/gardener-extension-provider-azure/pkg/azure/client"
+	"github.com/gardener/gardener-extension-provider-azure/pkg/controller/infrastructure/infraflow/shared"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"github.com/gardener/gardener/pkg/utils/flow"
+	"k8s.io/utils/pointer"
 )
 
 type FlowReconciler struct {
@@ -17,33 +20,109 @@ type FlowReconciler struct {
 
 func (f FlowReconciler) Reconcile(ctx context.Context, infra *extensionsv1alpha1.Infrastructure, cfg *azure.InfrastructureConfig) error {
 	//infrastructure.ComputeTerraformerTemplateValues(infra,cfg,) // use for migration of values..
-	if cfg.ResourceGroup != nil {
-		rgClient, err := f.Factory.ResourceGroup()
-		if err != nil {
-			return err
-		}
-		err = rgClient.CreateOrUpdate(ctx, cfg.ResourceGroup.Name, infra.Spec.Region)
-		var log = logf.Log.WithName("gardener-extension-admission-azure")
-		log.Info("Created resource group", *cfg.Networks.VNet.Name)
+	g := f.buildReconcileGraph(ctx, infra, cfg)
+	fl := g.Compile()
+	if err := fl.Run(ctx, flow.Opts{}); err != nil {
+		return flow.Causes(err)
+	}
+	return nil
+}
 
-		if err == nil {
-			vnetClient, err := f.Factory.Vnet()
+func (f FlowReconciler) buildReconcileGraph(ctx context.Context, infra *extensionsv1alpha1.Infrastructure, cfg *azure.InfrastructureConfig) *flow.Graph {
+	g := flow.NewGraph("Azure infrastructure reconcilation")
+
+	createResourceGroup := func(ctx context.Context) error {
+		if cfg.ResourceGroup != nil {
+			rgClient, err := f.Factory.ResourceGroup()
 			if err != nil {
 				return err
 			}
-			if cfg.Networks.VNet.Name != nil {
-				parameters := armnetwork.VirtualNetwork{
-					Location: to.Ptr(infra.Spec.Region),
-					Properties: &armnetwork.VirtualNetworkPropertiesFormat{
-						AddressSpace: &armnetwork.AddressSpace{AddressPrefixes: []*string{cfg.Networks.VNet.CIDR}},
-					},
-				}
-				err := vnetClient.CreateOrUpdate(ctx, cfg.ResourceGroup.Name, *cfg.Networks.VNet.Name, parameters)
-				log.Info("Created Vnet", *cfg.Networks.VNet.Name)
-				return err
-			}
+			return rgClient.CreateOrUpdate(ctx, cfg.ResourceGroup.Name, infra.Spec.Region)
+		}
+		return nil
+	}
+	resourceGroup := f.AddTask(g, "resource group creation", createResourceGroup)
 
+	createVnet := func(ctx context.Context) error {
+		vnetClient, err := f.Factory.Vnet()
+		if err != nil {
+			return err
+		}
+		if cfg.Networks.VNet.Name != nil {
+			parameters := armnetwork.VirtualNetwork{
+				Location: to.Ptr(infra.Spec.Region),
+				Properties: &armnetwork.VirtualNetworkPropertiesFormat{
+					AddressSpace: &armnetwork.AddressSpace{AddressPrefixes: []*string{cfg.Networks.VNet.CIDR}},
+				},
+			}
+			err := vnetClient.CreateOrUpdate(ctx, cfg.ResourceGroup.Name, *cfg.Networks.VNet.Name, parameters)
+			//log.Info("Created Vnet", *cfg.Networks.VNet.Name)
+			return err
+		}
+		return nil
+	}
+	f.AddTask(g, "vnet creation", createVnet, shared.Dependencies(resourceGroup))
+	return g
+
+}
+
+// AddTask adds a wrapped task for the given task function and options.
+func (c FlowReconciler) AddTask(g *flow.Graph, name string, fn flow.TaskFn, options ...shared.TaskOption) flow.TaskIDer {
+	allOptions := shared.TaskOption{}
+	for _, opt := range options {
+		if len(opt.Dependencies) > 0 {
+			allOptions.Dependencies = append(allOptions.Dependencies, opt.Dependencies...)
+		}
+		if opt.Timeout > 0 {
+			allOptions.Timeout = opt.Timeout
+		}
+		if opt.DoIf != nil {
+			condition := true
+			if allOptions.DoIf != nil {
+				condition = *allOptions.DoIf
+			}
+			condition = condition && *opt.DoIf
+			allOptions.DoIf = pointer.Bool(condition)
 		}
 	}
-	return nil
+
+	tunedFn := fn
+	if allOptions.DoIf != nil {
+		tunedFn = tunedFn.DoIf(*allOptions.DoIf)
+		if !*allOptions.DoIf {
+			name = "[Skipped] " + name
+		}
+	}
+	if allOptions.Timeout > 0 {
+		tunedFn = tunedFn.Timeout(allOptions.Timeout)
+	}
+	task := flow.Task{
+		Name: name,
+		Fn:   c.wrapTaskFn(g.Name(), name, tunedFn),
+	}
+
+	if len(allOptions.Dependencies) > 0 {
+		task.Dependencies = flow.NewTaskIDs(allOptions.Dependencies...)
+	}
+
+	return g.Add(task)
+}
+
+func (c FlowReconciler) wrapTaskFn(flowName, taskName string, fn flow.TaskFn) flow.TaskFn {
+	return func(ctx context.Context) error {
+		//taskCtx := logf.IntoContext(ctx, c.Log.WithValues("flow", flowName, "task", taskName))
+		err := fn(ctx) //fn(taskCtx)
+		if err != nil {
+			// don't wrap error with '%w', as otherwise the error context get lost
+			err = fmt.Errorf("failed to %s: %s", taskName, err)
+		}
+		//if perr := c.PersistState(taskCtx, false); perr != nil {
+		//	if err != nil {
+		//		c.Log.Error(perr, "persisting state failed")
+		//	} else {
+		//		err = perr
+		//	}
+		//}
+		return err
+	}
 }
