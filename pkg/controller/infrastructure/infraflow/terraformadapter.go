6e350ff1313ac3b2da77e5e4c48cb403190e0932
Adrian Stobbe <adrian.stobbe@sap.com>
2022-11-09 13:13:05 +0100

    tf nat + naming logic

--- pkg/controller/infrastructure/infraflow/terraformadapter.go
+++ pkg/controller/infrastructure/infraflow/terraformadapter.go
@@ -3,6 +3,7 @@ package infraflow
 import (
 	"fmt"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/gardener/gardener-extension-provider-azure/pkg/apis/azure"
 	"github.com/gardener/gardener-extension-provider-azure/pkg/internal/infrastructure"
 	"github.com/gardener/gardener/extensions/pkg/controller"
@@ -53,28 +54,73 @@ func (t TerraformAdapter) Subnets() []subnetTf {
 		return res
 	}
 	for _, subnet := range rawSubnets.([]map[string]interface{}) {
-		name := t.ClusterName() + "-nodes"
-		isMigrated, isMultiSubnet := subnet["migrated"]
-		if isMultiSubnet {
-			if !isMigrated.(bool) {
-				name = fmt.Sprintf("%s-z%s", t.ClusterName(), subnet["name"])
-			}
-		}
+		name := t.subnetName(subnet)
 		cidr := subnet["cidr"].(string)
 		serviceEndpoints := subnet["serviceEndpoints"].([]string)
-		res = append(res, subnetTf{name, cidr, serviceEndpoints})
+		res = append(res, subnetTf{name, cidr, serviceEndpoints, false})
 	}
 	return res
 }
 
+func (t TerraformAdapter) subnetName(subnet map[string]interface{}) string {
+	name := t.ClusterName() + "-nodes"
+	isMigrated, isMultiSubnet := subnet["migrated"]
+	if isMultiSubnet {
+		if !isMigrated.(bool) {
+			name = fmt.Sprintf("%s-z%d", t.ClusterName(), subnet["name"].(int32))
+		}
+	}
+	return name
+}
+
+func (t TerraformAdapter) Nats() []natTf {
+	res := make([]natTf, 0)
+	rawSubnets := t.values["networks"].(map[string]interface{})["subnets"]
+	if rawSubnets == nil {
+		return res
+	}
+	for _, subnet := range rawSubnets.([]map[string]interface{}) {
+		name := t.natName(subnet)
+		subnetName := t.subnetName(subnet)
+
+		natRaw := subnet["natGateway"].(map[string]interface{})
+		var idleConnectionTimeoutMinutes *int32 = nil
+		if _, ok := natRaw["idleConnectionTimeoutMinutes"]; ok {
+			idleConnectionTimeoutMinutes = to.Ptr(natRaw["idleConnectionTimeoutMinutes"].(int32))
+		}
+		//cidr := subnet["cidr"].(string)
+		//serviceEndpoints := subnet["serviceEndpoints"].([]string)
+		res = append(res, natTf{name, subnetName, natRaw["enabled"].(bool), idleConnectionTimeoutMinutes})
+	}
+	return res
+}
+
+func (t TerraformAdapter) natName(subnet map[string]interface{}) string {
+	name := t.ClusterName() + "-nat-gateway"
+	isMigrated, isMultiSubnet := subnet["migrated"]
+	if isMultiSubnet {
+		if !isMigrated.(bool) {
+			name = fmt.Sprintf("%s-z%d", name, subnet["name"].(int32))
+		}
+	}
+	return name
+}
+
+type natTf struct {
+	name                         string
+	subnetName                   string
+	enabled                      bool
+	idleConnectionTimeoutMinutes *int32
+}
 type subnetTf struct {
-	name             string
-	cidr             string
-	serviceEndpoints []string
+	name              string
+	cidr              string
+	serviceEndpoints  []string
+	natGatewayEnabled bool
 }
 
 type vnetTf (map[string]interface{})
 
-//func (v vnetTf) CIDR() *string {
-//	return v["cidr"].(*string)
-//}
+func (v vnetTf) Name() string {
+	return v["name"].(string)
+}
