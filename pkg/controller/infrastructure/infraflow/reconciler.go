4f8a433c5f97c0f944d24847cf9f32efd865fde0
Adrian Stobbe <adrian.stobbe@sap.com>
2022-11-17 15:55:52 +0100

    remove unused fn

--- pkg/controller/infrastructure/infraflow/reconciler.go
+++ pkg/controller/infrastructure/infraflow/reconciler.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v4"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2"
 	"github.com/gardener/gardener-extension-provider-azure/pkg/apis/azure"
 	"github.com/gardener/gardener-extension-provider-azure/pkg/azure/client"
@@ -13,7 +12,6 @@ import (
 	"github.com/gardener/gardener/extensions/pkg/controller"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"github.com/gardener/gardener/pkg/utils/flow"
-	"golang.org/x/sync/errgroup"
 	"k8s.io/utils/pointer"
 )
 
@@ -36,142 +34,6 @@ func (f FlowReconciler) Reconcile(ctx context.Context, infra *extensionsv1alpha1
 		return flow.Causes(err)
 	}
 	return nil
-	// other approach
-	//return f.buildGoRoutineFlow(err, ctx, tfAdapter)
-
-}
-
-func (f FlowReconciler) buildGoRoutineFlow(err error, ctx context.Context, tfAdapter TerraformAdapter) error {
-	err = f.reconcileResourceGroupFromTf(ctx, tfAdapter)
-	if err != nil {
-		return err
-	}
-	securityGroupCh := make(chan armnetwork.SecurityGroupsClientCreateOrUpdateResponse)
-	routeTableCh := make(chan armnetwork.RouteTable)
-	ipCh := make(chan map[string]armnetwork.PublicIPAddressesClientCreateOrUpdateResponse)
-	natGatewayCh := make(chan map[string]armnetwork.NatGatewaysClientCreateOrUpdateResponse, 2)
-	g, ctx := errgroup.WithContext(ctx) //https://stackoverflow.com/questions/45500836/close-multiple-goroutine-if-an-error-occurs-in-one-in-go
-	g.Go(func() error {
-		resp, err := f.reconcilePublicIPsFromTf(ctx, tfAdapter)
-		ipCh <- resp
-		return err
-	})
-	g.Go(func() error {
-		// TODO create ddosProtectionPlan before? tf only supports reference.. no creation
-		return f.reconcileVnetFromTf(ctx, tfAdapter)
-	})
-	g.Go(func() error {
-		resp, err := f.reconcileRouteTablesFromTf(ctx, tfAdapter)
-		routeTableCh <- resp
-		return err
-	})
-	g.Go(func() error {
-		resp, err := f.reconcileSecurityGroupsFromTf(ctx, tfAdapter)
-		securityGroupCh <- resp
-		return err
-	})
-	g.Go(func() error {
-		return f.reconcileAvailabilitySetFromTf(ctx, tfAdapter)
-	})
-	g.Go(func() error {
-		resp, err := f.reconcileNatGatewaysFromTf(ctx, tfAdapter, <-ipCh)
-		natGatewayCh <- resp // TODO use https://betterprogramming.pub/how-to-broadcast-messages-in-go-using-channels-b68f42bdf32e https://stackoverflow.com/questions/36417199/how-to-broadcast-message-using-channel
-		//err = fmt.Errorf("nat gateway error")
-		return err
-	})
-	// TODO split dependent tasks into seperate group, use ctx to cancel?
-	//if err := g.Wait(); err != nil {
-	//return err
-	//}
-	//g, ctx := errgroup.WithContext(ctx) //https://stackoverflow.com/questions/45500836/close-multiple-goroutine-if-an-error-occurs-in-one-in-go
-	//err = fmt.Errorf("nat gateway error")
-	g.Go(func() error {
-		return f.reconcileSubnetsFromTf(ctx, tfAdapter, <-securityGroupCh, <-routeTableCh, <-natGatewayCh)
-	})
-	return g.Wait()
-}
-
-//func (f FlowReconciler) reconcileResourceGroup(infra *extensionsv1alpha1.Infrastructure, cfg *azure.InfrastructureConfig) flow.TaskFn {
-//	return func(ctx context.Context) error {
-//		if cfg.ResourceGroup != nil {
-//			rgClient, err := f.Factory.ResourceGroup()
-//			if err != nil {
-//				return err
-//			}
-//			return rgClient.CreateOrUpdate(ctx, cfg.ResourceGroup.Name, infra.Spec.Region)
-//		}
-//		return nil
-//	}
-//}
-
-func (f FlowReconciler) reconcileAvailabilitySetFromTf(ctx context.Context, tf TerraformAdapter) error {
-	if tf.isCreate(TfAvailabilitySet) {
-		asClient, err := f.Factory.AvailabilitySet()
-		if err != nil {
-			return err
-		}
-		parameters := armcompute.AvailabilitySet{
-			Location: to.Ptr(tf.Region()),
-			Properties: &armcompute.AvailabilitySetProperties{
-				PlatformFaultDomainCount:  to.Ptr(tf.CountFaultDomains()),
-				PlatformUpdateDomainCount: to.Ptr(tf.CountUpdateDomains()),
-			},
-			SKU: &armcompute.SKU{Name: to.Ptr(string(armcompute.AvailabilitySetSKUTypesAligned))}, // equal to managed = True in tf
-		}
-		_, err = asClient.CreateOrUpdate(ctx, tf.ResourceGroup(), tf.AvailabilitySetName(), parameters)
-		return err
-	} else {
-		return nil
-	}
-}
-
-// res: subnet to ip mapping
-func (f FlowReconciler) reconcilePublicIPsFromTf(ctx context.Context, tf TerraformAdapter) (map[string]armnetwork.PublicIPAddressesClientCreateOrUpdateResponse, error) {
-	res := make(map[string]armnetwork.PublicIPAddressesClientCreateOrUpdateResponse)
-	client, err := f.Factory.PublicIP()
-	if err != nil {
-		return res, err
-	}
-	for _, ip := range tf.IPs() {
-		resp, err := client.CreateOrUpdate(ctx, tf.ResourceGroup(), ip.name, armnetwork.PublicIPAddress{
-			Location: to.Ptr(tf.Region()),
-			SKU:      &armnetwork.PublicIPAddressSKU{Name: to.Ptr(armnetwork.PublicIPAddressSKUNameStandard)},
-			Properties: &armnetwork.PublicIPAddressPropertiesFormat{
-				PublicIPAllocationMethod: to.Ptr(armnetwork.IPAllocationMethodStatic),
-			},
-			// TODO zones prop?
-		})
-		if err != nil {
-			return res, err
-		}
-		res[ip.subnetName] = resp
-	}
-	return res, nil
-}
-
-// res: subnet to nat mapping
-func (f FlowReconciler) reconcileNatGatewaysFromTf(ctx context.Context, tf TerraformAdapter, ips map[string]armnetwork.PublicIPAddressesClientCreateOrUpdateResponse) (res map[string]armnetwork.NatGatewaysClientCreateOrUpdateResponse, err error) {
-	res = make(map[string]armnetwork.NatGatewaysClientCreateOrUpdateResponse)
-	client, err := f.Factory.NatGateway()
-	if err != nil {
-		return res, err
-	}
-	nats := tf.Nats()
-	for _, nat := range nats {
-		if !nat.enabled {
-			continue
-		}
-		resp, err := client.CreateOrUpdate(ctx, tf.ResourceGroup(), nat.name, armnetwork.NatGateway{
-			Properties: &armnetwork.NatGatewayPropertiesFormat{
-				PublicIPAddresses: []*armnetwork.SubResource{{ID: ips[nat.subnetName].ID}},
-			},
-		})
-		if err != nil {
-			return res, err
-		}
-		res[nat.subnetName] = resp
-	}
-	return res, nil
 }
 
 func flowTask(tf TerraformAdapter, fn func(context.Context, TerraformAdapter) error) flow.TaskFn {
@@ -202,15 +64,6 @@ func (f FlowReconciler) reconcileResourceGroupFromTf(ctx context.Context, tf Ter
 		return err
 	}
 	return rgClient.CreateOrUpdate(ctx, tf.ResourceGroup(), tf.Region())
-}
-
-func (f FlowReconciler) reconcileVnetFromTf(ctx context.Context, tf TerraformAdapter) error {
-	vnetClient, err := f.Factory.Vnet()
-	if err != nil {
-		return err
-	}
-	return ReconcileVnetFromTf(ctx, tf, vnetClient)
-	//log.Info("Created Vnet", *cfg.Networks.VNet.Name)
 }
 
 func ReconcileVnetFromTf(ctx context.Context, tf TerraformAdapter, vnetClient client.Vnet) error {
@@ -259,18 +112,6 @@ func ReconcileVnetFromTf(ctx context.Context, tf TerraformAdapter, vnetClient cl
 //	}
 //}
 
-func (f FlowReconciler) reconcileRouteTablesFromTf(ctx context.Context, tf TerraformAdapter) (armnetwork.RouteTable, error) {
-	rclient, err := f.Factory.RouteTables()
-	if err != nil {
-		return armnetwork.RouteTable{}, err
-	}
-	// #TODO set in infraconfig? (default injection)
-	//Properties: &armnetwork.RouteTablePropertiesFormat{
-	//},
-	//log.Info("Created Vnet", *cfg.Networks.VNet.Name)
-	return ReconcileRouteTablesFromTf(tf, rclient, ctx)
-}
-
 func ReconcileRouteTablesFromTf(tf TerraformAdapter, rclient client.RouteTables, ctx context.Context) (armnetwork.RouteTable, error) {
 	routeTableName := "worker_route_table"
 	parameters := armnetwork.RouteTable{
@@ -281,58 +122,6 @@ func ReconcileRouteTablesFromTf(tf TerraformAdapter, rclient client.RouteTables,
 	return resp.RouteTable, err
 }
 
-func (f FlowReconciler) reconcileRouteTables(infra *extensionsv1alpha1.Infrastructure, cfg *azure.InfrastructureConfig) flow.TaskFn {
-	return func(ctx context.Context) error {
-		rclient, err := f.Factory.RouteTables()
-		routeTableName := "worker_route_table" // #TODO set in infraconfig? (default injection)
-		if err != nil {
-			return err
-		}
-		parameters := armnetwork.RouteTable{
-			Location: to.Ptr(infra.Spec.Region),
-			//Properties: &armnetwork.RouteTablePropertiesFormat{
-			//},
-		}
-		_, err = rclient.CreateOrUpdate(ctx, cfg.ResourceGroup.Name, routeTableName, parameters)
-		//log.Info("Created Vnet", *cfg.Networks.VNet.Name)
-		return err
-	}
-}
-
-func (f FlowReconciler) reconcileSecurityGroups(infra *extensionsv1alpha1.Infrastructure, cfg *azure.InfrastructureConfig) flow.TaskFn {
-	return func(ctx context.Context) error {
-		rclient, err := f.Factory.SecurityGroups()
-		if infra.Namespace == "" { // TODO validate before?
-			return fmt.Errorf("namespace is empty")
-		}
-		name := infra.Namespace + "-workers" // #TODO set in infraconfig? (default injection)
-		if err != nil {
-			return err
-		}
-		parameters := armnetwork.SecurityGroup{
-			Location: to.Ptr(infra.Spec.Region),
-			//Properties: &armnetwork.SecurityGroupPropertiesFormat{
-			//},
-		}
-		_, err = rclient.CreateOrUpdate(ctx, cfg.ResourceGroup.Name, name, parameters)
-		//log.Info("Created Vnet", *cfg.Networks.VNet.Name)
-		return err
-	}
-}
-
-func (f FlowReconciler) reconcileSecurityGroupsFromTf(ctx context.Context, tf TerraformAdapter) (armnetwork.SecurityGroupsClientCreateOrUpdateResponse, error) {
-	rclient, err := f.Factory.SecurityGroups()
-	if err != nil {
-		return armnetwork.SecurityGroupsClientCreateOrUpdateResponse{}, err
-	}
-
-	// #TODO set in infraconfig? (default injection)
-	//Properties: &armnetwork.SecurityGroupPropertiesFormat{
-	//},
-	//log.Info("Created Vnet", *cfg.Networks.VNet.Name)
-	return ReconcileSecurityGroupsFromTf(tf, rclient, ctx)
-}
-
 func ReconcileSecurityGroupsFromTf(tf TerraformAdapter, rclient client.SecurityGroups, ctx context.Context) (armnetwork.SecurityGroupsClientCreateOrUpdateResponse, error) {
 	name := tf.ClusterName() + "-workers"
 	parameters := armnetwork.SecurityGroup{
@@ -341,78 +130,6 @@ func ReconcileSecurityGroupsFromTf(tf TerraformAdapter, rclient client.SecurityG
 	resp, err := rclient.CreateOrUpdate(ctx, tf.ResourceGroup(), name, parameters)
 
 	return resp, err
-}
-
-func (f FlowReconciler) reconcileSubnetsFromTf(ctx context.Context, tf TerraformAdapter, securityGroup armnetwork.SecurityGroupsClientCreateOrUpdateResponse, routeTable armnetwork.RouteTable, nats map[string]armnetwork.NatGatewaysClientCreateOrUpdateResponse) error {
-	subnetClient, err := f.Factory.Subnet()
-	if err != nil {
-		return err
-	}
-	subnets := tf.Subnets()
-	for _, subnet := range subnets {
-		endpoints := make([]*armnetwork.ServiceEndpointPropertiesFormat, 0)
-		for _, endpoint := range subnet.serviceEndpoints {
-			endpoints = append(endpoints, &armnetwork.ServiceEndpointPropertiesFormat{
-				Service: to.Ptr(endpoint),
-			})
-		}
-
-		parameters := armnetwork.Subnet{
-			//Name: to.Ptr(subnet.name),
-			Properties: &armnetwork.SubnetPropertiesFormat{
-				AddressPrefix:    to.Ptr(subnet.cidr),
-				ServiceEndpoints: endpoints, // TODO associate security group?, route table?
-				NetworkSecurityGroup: &armnetwork.SecurityGroup{
-					ID: securityGroup.ID,
-				},
-				RouteTable: &armnetwork.RouteTable{
-					ID: routeTable.ID,
-				},
-				NatGateway: &armnetwork.SubResource{
-					ID: nats[subnet.name].ID,
-				},
-			},
-		}
-		err = subnetClient.CreateOrUpdate(ctx, tf.ResourceGroup(), tf.Vnet().Name(), subnet.name, parameters)
-	}
-	//log.Info("Created Vnet", *cfg.Networks.VNet.Name)
-	return err
-}
-
-func (f FlowReconciler) flowreconcileSubnetsFromTf(tf TerraformAdapter) flow.TaskFn {
-	return func(ctx context.Context) error {
-		subnetClient, err := f.Factory.Subnet()
-		if err != nil {
-			return err
-		}
-		name := "worker" // #TODO asset in infraconfig? (default injection)
-		subnets := tf.Subnets()
-		for _, subnet := range subnets {
-			endpoints := make([]*armnetwork.ServiceEndpointPropertiesFormat, 0)
-			for _, endpoint := range subnet.serviceEndpoints {
-				endpoints = append(endpoints, &armnetwork.ServiceEndpointPropertiesFormat{
-					Service: to.Ptr(endpoint),
-				})
-			}
-
-			parameters := armnetwork.Subnet{
-				Name: to.Ptr(subnet.name),
-				Properties: &armnetwork.SubnetPropertiesFormat{
-					AddressPrefix:    to.Ptr(subnet.cidr),
-					ServiceEndpoints: endpoints, // TODO associate security group?, route table?
-					//NetworkSecurityGroup: &armnetwork.SecurityGroup{
-					//	ID: to.Ptr(tf.SecurityGroup()["id"].(string)),
-					//},
-					//RouteTable: &armnetwork.SubResource{
-					//	ID: to.Ptr(tf.RouteTable()["id"].(string)),
-					//},
-				},
-			}
-			err = subnetClient.CreateOrUpdate(ctx, tf.ResourceGroup(), tf.Vnet()["name"].(string), name, parameters)
-		}
-		//log.Info("Created Vnet", *cfg.Networks.VNet.Name)
-		return err
-	}
 }
 
 func flowTaskNew[T any](clientFn func() (T, error), reconcileFn func(ctx context.Context, client T) error) flow.TaskFn {
