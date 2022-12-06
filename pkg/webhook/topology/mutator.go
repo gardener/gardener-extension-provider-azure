bd051273a40638e173bdf1125c9612a2d0ae2728
Konstantinos Angelopoulos <konstantinos.angelopoulos@sap.com>
2022-12-06 08:39:53 +0100

    add topology webhook (#602)
    * add topology-webhook
    
    * improve logging, comments
    
    * remove logger
    
    * updates are no-op

---
+++ pkg/webhook/topology/mutator.go
@@ -0,0 +1,109 @@
+//  Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package topology
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	gcontext "github.com/gardener/gardener/extensions/pkg/webhook/context"
+	"github.com/gardener/gardener/pkg/extensions"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type mutator struct {
+	client client.Client
+}
+
+// New initializes a new topology mutator that is responsible for adjusting the node affinity of pods.
+// The LabelTopologyZone label that Azure CCM adds to nodes does not contain only the zone as it appears in Azure API
+// calls but also the region like "$region-$zone". When only "$zone" is present for the LabelTopologyZone selector key
+// this mutator will adapt it to match the format that is used by the CCM labels.
+func New() *mutator {
+	return &mutator{}
+}
+
+func (m *mutator) InjectClient(client client.Client) error {
+	m.client = client
+	return nil
+}
+
+func (m *mutator) Mutate(ctx context.Context, new, old client.Object) error {
+	// do not try to mutate pods that are getting deleted
+	if new.GetDeletionTimestamp() != nil {
+		return nil
+	}
+
+	// Check if this is a create or update operation and perform no-op for updates
+	// Because the NodeAffinity of pods is an immutable field, we don't want to try and mutate it if the pod is already existing.
+	// TODO(KA): Remove once there is support for specifying webhook verbs
+	if old != nil {
+		return nil
+	}
+
+	newPod, ok := new.(*corev1.Pod)
+	if !ok {
+		return fmt.Errorf("object is not of type Pod")
+	}
+
+	gctx := gcontext.NewGardenContext(m.client, new)
+	cluster, err := gctx.GetCluster(ctx)
+	if err != nil {
+		return err
+	}
+	return m.mutateNodeAffinity(newPod, cluster)
+}
+
+func (m *mutator) mutateNodeAffinity(pod *corev1.Pod, cluster *extensions.Cluster) error {
+	if pod.Spec.Affinity == nil {
+		return nil
+	}
+
+	if pod.Spec.Affinity.NodeAffinity == nil {
+		return nil
+	}
+
+	if req := pod.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution; req != nil {
+		adaptNodeSelectorTermSlice(req.NodeSelectorTerms, cluster.Seed.Spec.Provider.Region)
+	}
+	if pref := pod.Spec.Affinity.NodeAffinity.PreferredDuringSchedulingIgnoredDuringExecution; pref != nil {
+		for p := range pref {
+			adaptNodeSelectorTerm(&pref[p].Preference, cluster.Seed.Spec.Provider.Region)
+		}
+	}
+
+	return nil
+
+}
+
+func adaptNodeSelectorTermSlice(terms []corev1.NodeSelectorTerm, region string) {
+	for termsIdx := range terms {
+		adaptNodeSelectorTerm(&terms[termsIdx], region)
+	}
+}
+
+func adaptNodeSelectorTerm(term *corev1.NodeSelectorTerm, region string) {
+	for _, expr := range term.MatchExpressions {
+		if expr.Key == corev1.LabelTopologyZone {
+			for idx, val := range expr.Values {
+				if !strings.HasPrefix(val, region) {
+					expr.Values[idx] = fmt.Sprintf("%s-%s", region, val)
+				}
+			}
+		}
+	}
+}
