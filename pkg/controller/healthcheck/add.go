a6233c4e6280d2eb22c0817e1245555c3192dcd4
Rafael Franzke <rafael.franzke@sap.com>
2023-06-21 13:27:29 +0200

    Adapt `healthcheck` controller

--- pkg/controller/healthcheck/add.go
+++ pkg/controller/healthcheck/add.go
@@ -113,6 +113,25 @@ func RegisterHealthChecks(mgr manager.Manager, opts healthcheck.DefaultAddArgs) 
 		return err
 	}
 
+	var (
+		workerHealthChecks = []healthcheck.ConditionTypeToHealthCheck{{
+			ConditionType: string(gardencorev1beta1.ShootEveryNodeReady),
+			HealthCheck:   worker.NewNodesChecker(),
+			ErrorCodeCheckFunc: func(err error) []gardencorev1beta1.ErrorCode {
+				return util.DetermineErrorCodes(err, helper.KnownCodes)
+			},
+		}}
+		workerConditionTypesToRemove = sets.New(gardencorev1beta1.ShootControlPlaneHealthy)
+	)
+
+	if !GardenletManagesMCM {
+		workerHealthChecks = append(workerHealthChecks, healthcheck.ConditionTypeToHealthCheck{
+			ConditionType: string(gardencorev1beta1.ShootControlPlaneHealthy),
+			HealthCheck:   general.NewSeedDeploymentHealthChecker(azure.MachineControllerManagerName),
+		})
+		workerConditionTypesToRemove = workerConditionTypesToRemove.Delete(gardencorev1beta1.ShootControlPlaneHealthy)
+	}
+
 	return healthcheck.DefaultRegistration(
 		azure.Type,
 		extensionsv1alpha1.SchemeGroupVersion.WithKind(extensionsv1alpha1.WorkerResource),
@@ -121,20 +140,8 @@ func RegisterHealthChecks(mgr manager.Manager, opts healthcheck.DefaultAddArgs) 
 		mgr,
 		opts,
 		nil,
-		[]healthcheck.ConditionTypeToHealthCheck{
-			{
-				ConditionType: string(gardencorev1beta1.ShootControlPlaneHealthy),
-				HealthCheck:   general.NewSeedDeploymentHealthChecker(azure.MachineControllerManagerName),
-			},
-			{
-				ConditionType: string(gardencorev1beta1.ShootEveryNodeReady),
-				HealthCheck:   worker.NewNodesChecker(),
-				ErrorCodeCheckFunc: func(err error) []gardencorev1beta1.ErrorCode {
-					return util.DetermineErrorCodes(err, helper.KnownCodes)
-				},
-			},
-		},
-		sets.Set[gardencorev1beta1.ConditionType]{},
+		workerHealthChecks,
+		workerConditionTypesToRemove,
 	)
 }
 
