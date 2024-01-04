959e615f334e200ee8018bcde43e93943d5e15a8
Tim Usner <tim.usner@sap.com>
2024-01-04 09:43:14 +0100

    Adjust paths after vendor removal

--- charts/gardener-extension-provider-azure/doc.go
+++ charts/gardener-extension-provider-azure/doc.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate sh -c "../../vendor/github.com/gardener/gardener/hack/generate-controller-registration.sh provider-azure . $(cat ../../VERSION) ../../example/controller-registration.yaml BackupBucket:azure BackupEntry:azure Bastion:azure ControlPlane:azure DNSRecord:azure-dns Infrastructure:azure Worker:azure"
+//go:generate sh -c "bash $GARDENER_HACK_DIR/generate-controller-registration.sh provider-azure . $(cat ../../VERSION) ../../example/controller-registration.yaml BackupBucket:azure BackupEntry:azure Bastion:azure ControlPlane:azure DNSRecord:azure-dns Infrastructure:azure Worker:azure"
 
 // Package chart enables go:generate support for generating the correct controller registration.
 package chart
