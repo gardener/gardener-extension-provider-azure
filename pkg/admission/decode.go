d99fcce5a6725bfbd93a700c235c885c5b8a8661
Dimitar Mirchev <dimitar.mirchev@sap.com>
2024-11-01 14:55:05 +0200

    Add DecodeWorkloadIdentityConfig helper

---
+++ pkg/admission/decode.go
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package admission
+
+import (
+	"github.com/gardener/gardener-extension-provider-azure/pkg/apis/azure"
+	"github.com/gardener/gardener/extensions/pkg/util"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+// DecodeWorkloadIdentityConfig decodes the `WorkloadIdentityConfig` from the given `RawExtension`.
+func DecodeWorkloadIdentityConfig(decoder runtime.Decoder, config *runtime.RawExtension) (*azure.WorkloadIdentityConfig, error) {
+	workloadIdentityConfig := &azure.WorkloadIdentityConfig{}
+	if err := util.Decode(decoder, config.Raw, workloadIdentityConfig); err != nil {
+		return nil, err
+	}
+
+	return workloadIdentityConfig, nil
+}
