8709dd94fe073cb1b64fc7db512fa3e9b3bd1e31
Andreas Burger <andreas.burger@sap.com>
2025-02-20 12:42:28 +0100

    appease linter

--- pkg/azure/types.go
+++ pkg/azure/types.go
@@ -162,9 +162,8 @@ const (
 	// WorkloadIdentityTokenFileKey is the key indicating the full path to the workload identity token file.
 	WorkloadIdentityTokenFileKey = "workloadIdentityTokenFile"
 
-	// TODO (AndreasBurger): Clean up once SnapshotValidation is removed everywhere
 	// CSISnapshotValidationName is the constant for the name of the csi-snapshot-validation-webhook component.
-	CSISnapshotValidationName = "csi-snapshot-validation"
+	CSISnapshotValidationName = "csi-snapshot-validation" // TODO(AndreasBurger): Clean up once SnapshotValidation is removed everywhere
 )
 
 // UsernamePrefix is a constant for the username prefix of components deployed by Azure.
