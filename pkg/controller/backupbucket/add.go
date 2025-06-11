82c4d3efdd2150bdf8e48f0be4128a0fc9fb9deb
Shafeeque E S <shafeeque.e.s@sap.com>
2025-06-11 11:28:57 +0530

    Add a predicate to reconcile the extension for StorageAccountKeyMustRotate annotation (#1190)

--- pkg/controller/backupbucket/add.go
+++ pkg/controller/backupbucket/add.go
@@ -9,8 +9,12 @@ import (
 
 	"github.com/gardener/gardener/extensions/pkg/controller/backupbucket"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	predicateutils "github.com/gardener/gardener/pkg/controllerutils/predicate"
+	kutil "github.com/gardener/gardener/pkg/utils/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
 	"github.com/gardener/gardener-extension-provider-azure/pkg/azure"
 )
@@ -36,7 +40,7 @@ func AddToManagerWithOptions(_ context.Context, mgr manager.Manager, opts AddOpt
 	return backupbucket.Add(mgr, backupbucket.AddArgs{
 		Actuator:          NewActuator(mgr),
 		ControllerOptions: opts.Controller,
-		Predicates:        backupbucket.DefaultPredicates(opts.IgnoreOperationAnnotation),
+		Predicates:        getPredicates(opts),
 		Type:              azure.Type,
 		ExtensionClass:    opts.ExtensionClass,
 	})
@@ -45,4 +49,20 @@ func AddToManagerWithOptions(_ context.Context, mgr manager.Manager, opts AddOpt
 // AddToManager adds a controller with the default Options.
 func AddToManager(ctx context.Context, mgr manager.Manager) error {
 	return AddToManagerWithOptions(ctx, mgr, DefaultAddOptions)
+}
+
+func getPredicates(opts AddOptions) []predicate.Predicate {
+	defaultPredicates := predicate.And(backupbucket.DefaultPredicates(opts.IgnoreOperationAnnotation)...)
+
+	// Trigger reconcile if StorageAccountKeyMustRotate annotation is set to 'true' and the event is Create or Update.
+	storageAccountKeyMustRotatePredicate := predicate.And(predicate.NewPredicateFuncs(func(obj client.Object) bool {
+		backupBucket, ok := obj.(*extensionsv1alpha1.BackupBucket)
+		if !ok {
+			return false
+		}
+
+		return kutil.HasMetaDataAnnotation(backupBucket, azure.StorageAccountKeyMustRotate, "true")
+	}), predicateutils.ForEventTypes(predicateutils.Create, predicateutils.Update))
+
+	return []predicate.Predicate{predicate.Or(defaultPredicates, storageAccountKeyMustRotatePredicate)}
 }
