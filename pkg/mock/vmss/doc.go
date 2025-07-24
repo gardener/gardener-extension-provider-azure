5a324dfbc19ef160ccbd3f0a2fc24fe868c41170
Alexander Hebel <a.hebelsun@gmail.com>
2025-07-24 09:51:10 +0200

    REUSE: Convert DEP5 to REUSE.toml (#1224)
    * reuse convert-dep5
    
    * Update SPDX-FileCopyrightText
    
    * Update SPDX-FileCopyrightText

--- pkg/mock/vmss/doc.go
+++ pkg/mock/vmss/doc.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
 //
 // SPDX-License-Identifier: Apache-2.0
 //go:generate mockgen -package vmss -destination=mocks.go github.com/gardener/gardener-extension-provider-azure/pkg/azure/client Vmss
