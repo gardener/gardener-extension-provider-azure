f8d11e36b213d73e8fd70ee014de481ceed95978
Alexander Hebel <a.hebelsun@gmail.com>
2025-09-09 14:13:03 +0200

    Add forceNatGateway flag

--- cmd/gardener-extension-admission-azure/app/app.go
+++ cmd/gardener-extension-admission-azure/app/app.go
@@ -30,10 +30,13 @@ import (
 	admissioncmd "github.com/gardener/gardener-extension-provider-azure/pkg/admission/cmd"
 	azureinstall "github.com/gardener/gardener-extension-provider-azure/pkg/apis/azure/install"
 	providerazure "github.com/gardener/gardener-extension-provider-azure/pkg/azure"
+	"github.com/gardener/gardener-extension-provider-azure/pkg/features"
 )
 
 // AdmissionName is the name of the admission component.
 const AdmissionName = "admission-azure"
+
+var forceNatGatewayFlag bool
 
 var log = logf.Log.WithName("gardener-extension-admission-azure")
 
@@ -76,6 +79,13 @@ func NewAdmissionCommand(ctx context.Context) *cobra.Command {
 
 		RunE: func(_ *cobra.Command, _ []string) error {
 			verflag.PrintAndExitIfRequested()
+
+			if forceNatGatewayFlag {
+				err := features.ExtensionFeatureGate.Set(fmt.Sprintf("%s=%s", features.ForceNatGateway, "true"))
+				if err != nil {
+					return err
+				}
+			}
 
 			if gardenKubeconfig := os.Getenv("GARDEN_KUBECONFIG"); gardenKubeconfig != "" {
 				log.Info("Getting rest config for garden from GARDEN_KUBECONFIG", "path", gardenKubeconfig)
@@ -162,6 +172,7 @@ func NewAdmissionCommand(ctx context.Context) *cobra.Command {
 		},
 	}
 
+	cmd.Flags().BoolVar(&forceNatGatewayFlag, "force-nat-gateway", false, "Force the usage of NAT Gateways")
 	verflag.AddFlags(cmd.Flags())
 	aggOption.AddFlags(cmd.Flags())
 
