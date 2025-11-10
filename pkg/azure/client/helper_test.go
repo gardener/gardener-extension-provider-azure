96dc6f873d1554f39cfab1f393bd7a20ac03270a
Vladimir Nachev <vladimir.nachev@sap.com>
2025-11-10 13:37:02 +0200

    Add tests for `pkg/azure/client.CloudConfiguration` (#1360)
    * Add tests for pkg/azure/client.CloudConfiguration
    
    * Add tests for AzureCloudConfiguration and AzureCloudConfigurationFromCloudConfiguration

--- pkg/azure/client/helper_test.go
+++ pkg/azure/client/helper_test.go
@@ -9,10 +9,13 @@ import (
 	"net/http"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
 	azerrors "github.com/AzureAD/microsoft-authentication-library-for-go/apps/errors"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"k8s.io/utils/ptr"
 
+	"github.com/gardener/gardener-extension-provider-azure/pkg/apis/azure"
 	. "github.com/gardener/gardener-extension-provider-azure/pkg/azure/client"
 )
 
@@ -23,9 +26,13 @@ var _ = Describe("Helper", func() {
 		},
 		Entry("should return false as error is not a detailed azure error", errors.New("error"), false),
 		Entry("should return true as error is a NotFound error",
-			&azcore.ResponseError{StatusCode: http.StatusNotFound}, true),
+			&azcore.ResponseError{StatusCode: http.StatusNotFound}, true,
+		),
 		Entry("should return true as error is a NotFound error",
-			azerrors.CallErr{Resp: &http.Response{StatusCode: http.StatusNotFound}}, true))
+			azerrors.CallErr{Resp: &http.Response{StatusCode: http.StatusNotFound}}, true,
+		),
+	)
+
 	DescribeTable("#IsAzureAPIUnauthorized",
 		func(errorType int, statusCode int, expectIsUnauthorizedError bool) {
 			err := errors.New("error")
@@ -50,5 +57,69 @@ var _ = Describe("Helper", func() {
 		Entry("should return true as error if it is an Unauthorized call error", 1, http.StatusUnauthorized, true),
 		Entry("should return false as error if it is an NotFound call error", 1, http.StatusNotFound, false),
 		Entry("should return false as error if it is an unknown error", -1, http.StatusUnauthorized, false),
+	)
+
+	DescribeTable("#CloudConfiguration",
+		func(cc *azure.CloudConfiguration, region *string, expectedCloud string, expectedError error) {
+			cloud, err := CloudConfiguration(cc, region)
+			if expectedError != nil {
+				Expect(err).To(MatchError(expectedError))
+				Expect(cloud).To(BeNil())
+			} else {
+				Expect(err).ToNot(HaveOccurred())
+				Expect(cloud).ToNot(BeNil())
+				Expect(cloud.Name).To(Equal(expectedCloud))
+			}
+		},
+		Entry("should resolve cloud configuration from  cloudConfiguration=AzurePublic", &azure.CloudConfiguration{Name: "AzurePublic"}, nil, "AzurePublic", nil),
+		Entry("should resolve cloud configuration from  cloudConfiguration=AzureChina", &azure.CloudConfiguration{Name: "AzureChina"}, nil, "AzureChina", nil),
+		Entry("should resolve cloud configuration from  cloudConfiguration=AzureGovernment", &azure.CloudConfiguration{Name: "AzureGovernment"}, nil, "AzureGovernment", nil),
+		Entry("should resolve cloud configuration from  region=AzurePublic", nil, ptr.To("eastus"), "AzurePublic", nil),
+		Entry("should resolve cloud configuration from  region=AzurePublic", nil, ptr.To("westeurope"), "AzurePublic", nil),
+		Entry("should resolve cloud configuration from  region=AzurePublic", nil, ptr.To("uksouth"), "AzurePublic", nil),
+		Entry("should resolve cloud configuration from  region=AzureChina", nil, ptr.To("chinanorth3"), "AzureChina", nil),
+		Entry("should resolve cloud configuration from  region=AzureChina", nil, ptr.To("chinaeast"), "AzureChina", nil),
+		Entry("should resolve cloud configuration from  region=AzureGovernment", nil, ptr.To("USGovTexas"), "AzureGovernment", nil),
+		Entry("should resolve cloud configuration from  region=AzureGovernment", nil, ptr.To("USDoDCentral"), "AzureGovernment", nil),
+		Entry("should resolve cloud configuration from  region=AzureGovernment", nil, ptr.To("USSecEast"), "AzureGovernment", nil),
+		Entry("should fail to resolve cloud configuration", nil, nil, "", errors.New("either CloudConfiguration or region must not be nil to determine Azure Cloud configuration")),
+	)
+
+	DescribeTable("#AzureCloudConfiguration",
+		func(cc *azure.CloudConfiguration, region *string, expectedCloud cloud.Configuration, expectedError error) {
+			cloudConfig, err := AzureCloudConfiguration(cc, region)
+			if expectedError != nil {
+				Expect(err).To(MatchError(expectedError))
+				Expect(cloudConfig).To(Equal(cloud.Configuration{}))
+			} else {
+				Expect(err).ToNot(HaveOccurred())
+				Expect(cloudConfig).To(Equal(expectedCloud))
+			}
+		},
+		Entry("should resolve AzurePublic cloud configuration from CloudConfiguration", &azure.CloudConfiguration{Name: azure.AzurePublicCloudName}, nil, cloud.AzurePublic, nil),
+		Entry("should resolve AzureChina cloud configuration from CloudConfiguration", &azure.CloudConfiguration{Name: azure.AzureChinaCloudName}, nil, cloud.AzureChina, nil),
+		Entry("should resolve AzureGovernment cloud configuration from CloudConfiguration", &azure.CloudConfiguration{Name: azure.AzureGovCloudName}, nil, cloud.AzureGovernment, nil),
+		Entry("should resolve AzurePublic cloud configuration from region", nil, ptr.To("eastus"), cloud.AzurePublic, nil),
+		Entry("should resolve AzureChina cloud configuration from region", nil, ptr.To("chinanorth3"), cloud.AzureChina, nil),
+		Entry("should resolve AzureGovernment cloud configuration from region", nil, ptr.To("USGovTexas"), cloud.AzureGovernment, nil),
+		Entry("should fail to resolve cloud configuration", nil, nil, cloud.Configuration{}, errors.New("either CloudConfiguration or region must not be nil to determine Azure Cloud configuration")),
+	)
+
+	DescribeTable("#AzureCloudConfigurationFromCloudConfiguration",
+		func(cc *azure.CloudConfiguration, expectedCloud cloud.Configuration, expectedError error) {
+			cloudConfig, err := AzureCloudConfigurationFromCloudConfiguration(cc)
+			if expectedError != nil {
+				Expect(err).To(MatchError(expectedError))
+				Expect(cloudConfig).To(Equal(cloud.Configuration{}))
+			} else {
+				Expect(err).ToNot(HaveOccurred())
+				Expect(cloudConfig).To(Equal(expectedCloud))
+			}
+		},
+		Entry("should return AzurePublic when input is nil", nil, cloud.AzurePublic, nil),
+		Entry("should return AzurePublic for AzurePublicCloudName", &azure.CloudConfiguration{Name: azure.AzurePublicCloudName}, cloud.AzurePublic, nil),
+		Entry("should return AzureGovernment for AzureGovCloudName", &azure.CloudConfiguration{Name: azure.AzureGovCloudName}, cloud.AzureGovernment, nil),
+		Entry("should return AzureChina for AzureChinaCloudName", &azure.CloudConfiguration{Name: azure.AzureChinaCloudName}, cloud.AzureChina, nil),
+		Entry("should return error for unknown cloud name", &azure.CloudConfiguration{Name: "UnknownCloud"}, cloud.Configuration{}, errors.New("unknown cloud configuration name 'UnknownCloud'")),
 	)
 })
