7a708fe14720f99137a9298891874548dd9b5cf3
Marc Vornetran <marc.vornetran@sap.com>
2026-08-26 16:54:21 +0200

    Fix copyright text: remove redundant "Copyright" prefix
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>

--- pkg/mock/vmss/doc.go
+++ pkg/mock/vmss/doc.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright Contributors to the Gardener project
+// SPDX-FileCopyrightText: Contributors to the Gardener project
 //
 // SPDX-License-Identifier: Apache-2.0
 //go:generate mockgen -package vmss -destination=mocks.go github.com/gardener/gardener-extension-provider-azure/pkg/azure/client Vmss
