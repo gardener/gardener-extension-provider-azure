341edb4a852e5781fb2d9f681669153d7c94717b
Marc Vornetran <marc.vornetran@sap.com>
2026-08-26 15:19:05 +0200

    Update copyright text
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>

--- pkg/mock/vmss/doc.go
+++ pkg/mock/vmss/doc.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+// SPDX-FileCopyrightText: Copyright Contributors to the Gardener project
 //
 // SPDX-License-Identifier: Apache-2.0
 //go:generate mockgen -package vmss -destination=mocks.go github.com/gardener/gardener-extension-provider-azure/pkg/azure/client Vmss
